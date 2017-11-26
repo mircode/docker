b8963d30bbc1a74dbcd97e46af899725f686dfbf
mircode <1607646162@qq.com>
2017-11-26 20:37:22 +0800

    docker drill

---
+++ java/drill/init.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./load.sh date=20160101-20170101 saveby=month file=/log_month
--- java/drill/start.sh
+++ java/drill/start.sh
@@ -5,6 +5,7 @@ export ZOO_LOG_DIR=$APP_HOME/zookeeper/logs
 
 # ln
 ln -s $APP_HOME/drill/bin/sql.sh ./sql
+ln -s $APP_HOME/drill/bin/load.sh ./load.sh
 
 # start
 ./zookeeper/bin/zkServer.sh start
